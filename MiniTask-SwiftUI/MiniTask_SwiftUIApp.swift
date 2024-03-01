478910fc87ccc1bbbe4fe125cdb013d85f252f08
Jimuel Renzo Medrano <jimuelrenzomedrano@gmail.com>
2024-03-01 20:14:54 +0800

    Initial Commit

---
+++ MiniTask-SwiftUI/MiniTask_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniTask_SwiftUIApp.swift
+//  MiniTask-SwiftUI
+//
+//  Created by Jimuel Renzo Medrano on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniTask_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
