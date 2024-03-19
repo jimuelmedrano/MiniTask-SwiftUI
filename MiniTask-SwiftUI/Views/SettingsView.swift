94450b63b82ed471f5b14f484f7aa3708131390e
Jimuel Renzo Medrano <jimuelrenzomedrano@gmail.com>
2024-03-19 23:17:43 +0800

    add dark mode toggle

---
+++ MiniTask-SwiftUI/Views/SettingsView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsView.swift
+//  MiniTask-SwiftUI
+//
+//  Created by Jimuel Renzo Medrano on 3/19/24.
+//  Copyright © 2024 jimuelmedrano. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
